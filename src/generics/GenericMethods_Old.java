43c90b77819052a4b947c07ee68e3d704b06b6d8
sergii-tsymbaliuk <ser9ey@gmail.com>
2015-12-24 00:37:04 +0200

    20151223 - generic, javapools

---
+++ src/generics/GenericMethods_Old.java
@@ -0,0 +1,24 @@
+//: generics/GenericMethods.java
+package generics; /* Added by Eclipse.py */
+
+public class GenericMethods_Old {
+  public <T> void f(T x) {
+    System.out.println(x.getClass().getName());
+  }
+  public static void main(String[] args) {
+    GenericMethods_Old gm = new GenericMethods_Old();
+    gm.f("");
+    gm.f(1);
+    gm.f(1.0);
+    gm.f(1.0F);
+    gm.f('c');
+    gm.f(gm);
+  }
+} /* Output:
+java.lang.String
+java.lang.Integer
+java.lang.Double
+java.lang.Float
+java.lang.Character
+GenericMethods
+*///:~
