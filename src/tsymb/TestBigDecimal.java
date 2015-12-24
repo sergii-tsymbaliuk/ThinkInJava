69446daa812a18b8ed712fbf8dc6141c252a68aa
Sergii_Tsymbaliuk <Sergii_Tsymbaliuk@EPUAKYIW7048.kyiv.epam.com>
2015-12-24 20:09:00 +0200

    TestStringPool & BigDecimal

---
+++ src/tsymb/TestBigDecimal.java
@@ -0,0 +1,10 @@
+package tsymb;
+
+import java.math.BigDecimal;
+
+public class TestBigDecimal {
+	public static void main(String args[]){
+		BigDecimal bd = new BigDecimal("100.3");
+		System.out.println(bd);
+	}
+}
--- src/tsymb/TestStringPool.java
+++ src/tsymb/TestStringPool.java
@@ -1,30 +1,43 @@
 package tsymb;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 interface TestAnonimousClass{
-	public void doSmtng (String s, TestAnonimousClass parent);
+	public TestAnonimousClass init (String s);
 }
 
 public class TestStringPool {
 	
 	public static void main (String args[]){
-		String s = "";
+		String s = new String("asdfasd");
 		new Scanner(System.in).nextLine();
-		TestAnonimousClass tac;
+		//TestAnonimousClass tac;
+		Random rnd  = new Random(47);
+		List<TestAnonimousClass> list = new ArrayList<TestAnonimousClass>();
 		
-		for (long i=0;i<1000000000;i++){
-			tac = new TestAnonimousClass(){
+		while(true){
+			s = "New Int: "  + rnd.nextLong();
+			list.add( new TestAnonimousClass(){
 				String s;
-				TestAnonimousClass parent;
-				
-				public void doSmtng(String s,TestAnonimousClass parent) {
-					this.s = s+" "+String.valueOf(s);
-					this.s.intern();
-				}
-			};
-			tac.doSmtng(s,tac);
-			//System.out.println(s);
+				public TestAnonimousClass init(String s) {
+					this.s = s;
+					return this;
+				};}.init(s));
 		}
+//			tac = new TestAnonimousClass(){
+//				String s;
+//				TestAnonimousClass parent;
+//				
+//				public void doSmtng(String s,TestAnonimousClass parent) {
+//					this.s = s+"kjhlkjh"+String.valueOf(s);
+//					this.s.intern();
+//				}
+//			};
+			//tac.doSmtng(s,tac);
+			//System.out.println(s);
+		//}
 	}
 }
